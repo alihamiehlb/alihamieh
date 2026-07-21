7a10f10e03536182ae36213e6ab312866e066605
alihamiehlb <alihamiehlb@gmail.com>
2026-07-21 22:35:07 +0300

    Fix sitemap build timeout by making it dynamic

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next';
 import { getSiteContent } from '@/lib/get-site-content';
 
+export const dynamic = 'force-dynamic';
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = 'https://alihamieh.com';
   const site = await getSiteContent();
